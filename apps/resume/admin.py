1dd19361f1e712077006abc9f87e535f15475af3
Gabriel <gabrielmrentero@gmail.com>
2019-03-07 17:55:02 +0000

    Adding new model Resume to admin

--- apps/resume/admin.py
+++ apps/resume/admin.py
@@ -3,7 +3,12 @@ from django.contrib import admin
 from . import models
 
 
+@admin.register(models.Resume)
+class Resume(admin.ModelAdmin):
+    list_display = ('title',)
+
+
 @admin.register(models.ResumeItem)
 class ResumeItemAdmin(admin.ModelAdmin):
-    list_display = ('user', 'title', 'company', 'start_date')
-    ordering = ('user', '-start_date')
+    list_display = ('resume', 'title', 'company', 'start_date')
+    ordering = ('resume', '-start_date')
